b4bb0ee022444c9c74ec1f683b293b6684d9affa
aastom <aastom@pm.me>
2023-01-15 02:34:23 +0000

    add kubernetes output
    also add circleci

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=aastom/predictions
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run hazelcast --image=aastom/predictions --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pods/hazelcast 8000:80
 
